5890724bd5427b39bf4396f76723cb7c545f3815
Sanjana Nilanka <nilankasanjana803@gmail.com>
2024-09-10 05:07:47 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import {Avatar, AvatarGroup, AvatarIcon} from "@nextui-org/react";
+function App() {
+
+  return (
+    <>
+      <Avatar src="https://i.pravatar.cc/150?u=a042581f4e29026024d" />
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
